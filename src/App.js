7c964f56f8a0fb2a13e78d3fb7688206dfe4d3a5
Samuel Kevin <samuel.laluyan@student.sgu.ac.id>
2024-07-14 18:41:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 
 function App() {
+
+  const [balance, setBalance] = useState(null);
+  const [contractAddress, setContractAddress] = useState('');
+
+  const getBalance = async () => {
+    try {
+      const provider = new ethers.providers.JsonRpcProvider('[INFURA_PROVIDER_RPC_URL_WITH_API_KEY]');
+      const balance = await provider.getBalance(contractAddress);
+      setBalance(ethers.utils.formatEther(balance));
+    } catch (error) {
+      console.error('Error fetching balance:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Check ETH Balance</h1>
+      <input
+        type="text"
+        placeholder="Enter Contract Address"
+        value={contractAddress}
+        onChange={(e) => setContractAddress(e.target.value)}
+      />
+      <button onClick={getBalance}>Get Balance</button>
+      {balance && (
+        <div>
+          <h2>Balance:</h2>
+          <p>{balance} ETH</p>
+        </div>
+      )}
     </div>
   );
 }
